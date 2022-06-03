ea0c2a2361ed900a3a451b80f7eefce9431bd118
ericmahare <erikmahare@gmail.com>
2022-06-04 01:25:50 +0300

    Add books component

---
+++ src/components/Books.js
@@ -0,0 +1,14 @@
+import Book from "./Book"
+import Form from "./Form"
+
+const Books = () => {
+  return (
+    <>
+      <Book/>
+      <Book/>
+      <Form/>
+    </>
+  )
+}
+
+export default Books
