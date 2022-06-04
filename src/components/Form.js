b34fcd3f6a90267f4afd2e42775614a5443c8bbe
ericmahare <erikmahare@gmail.com>
2022-06-04 10:21:36 +0300

    Change select input to text input

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,9 +4,7 @@ const Form = () => {
     <h4>ADD NEW BOOK</h4>
     <form>
       <input type='text' placeholder='book title'/>
-      <select id="cars" name="cars">
-        <option value="volvo">Author</option>
-      </select>
+      <input type='text' placeholder='Author'/>
       <button className="form-btn">Add book</button>
     </form>
     </div>
