8249d595f2f3ee258f89e468c30ae66e5f9e3a3a
ericmahare <erikmahare@gmail.com>
2022-06-07 12:32:08 +0300

    combine redux reducers

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,19 +8,24 @@ const Form = ({dispatch}) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if(title !== '' && author !== ''){
     const book = {
       id: shortid.generate(),
       title,
       author,
     }
-    dispatch(addBooks(book))
+    dispatch(addBooks(book));
+    setAuthor('')
+    setTitle('')
+  }
   }
   return (
     <div className="form-section">
     <h4>ADD NEW BOOK</h4>
     <form onSubmit={handleSubmit}>
       <input type='text' placeholder='book title' value={title} onChange={(e)=>setTitle(e.target.value)}/>
-      <input type='text' placeholder='Author' author={author} onChange={(e)=>setAuthor(e.target.value)}/>
+      <input type='text' placeholder='Author' value={author} onChange={(e)=>setAuthor(e.target.value)}/>
       <button className="form-btn">Add book</button>
     </form>
     </div>
--- src/redux/configurationStore.js
+++ src/redux/configurationStore.js
@@ -1,11 +1,14 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import logger from "redux-logger";
 import booksReducer from "./books/books";
+import categoryReducer from "./categories/categories";
 
 const rootReducer = combineReducers({
   books: booksReducer,
+  category: categoryReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 
 export default store
