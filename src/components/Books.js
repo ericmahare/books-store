eeab331e01fab4e9b640a243d0e8c2ee9a708cb9
ericmahare <erikmahare@gmail.com>
2022-06-09 13:55:08 +0300

    get store books data

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,13 +1,25 @@
-import { useSelector } from "react-redux"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchBooks } from "../redux/books/books"
 import Book from "./Book"
 import Form from "./Form"
 
 const Books = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchBooks())
+  },[])
   const books = useSelector(state => state.books)
-  let data = books.map(book => <Book key={book.id} book={book}/>);
   return (
     <>
-      {data}
+      {Object.keys(books).map((id) => (
+        <Book
+          key={id}
+          id={id}
+          title={books[id][0].title}
+          author={books[id][0].author}
+        />
+      ))}
       <Form/>
     </>
   )
